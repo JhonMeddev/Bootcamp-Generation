b41fb1d2d9b45c034e29964b3d3aabafb8e38520
Jhon Med <jhonatan1medeiros@gmail.com>
2021-11-12 16:27:48 -0300

    12NovExercicio

---
+++ 12NovExerciciosPOO/ex1/cliente2.java
@@ -0,0 +1,17 @@
+/*
+1) Crie uma classe cliente e apresente os atributos e métodos referentes
+esta classe, em seguida crie um objeto cliente, defina as instancias deste
+objeto e apresente as informações deste objeto no console.
+*/
+
+package 12NovExerciciosPOO.ex1;
+
+public class cliente2 {
+
+    String Nome;
+    int idade;
+    double: orçamento;
+
+    
+    
+}
